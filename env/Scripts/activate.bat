1f76d4fad533502074ed8bfb6c25079dfbf6bd61
asniel rodriguez ruiz <rasniel@yahoo.es>
2024-02-07 21:09:27 -0500

    delete

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\asnie\OneDrive\Desktop\LearnigDjango\ToDo\env
+set VIRTUAL_ENV=C:\Users\rasni\Desktop\ToDo_app\ToDo\env
 
 if not defined PROMPT set PROMPT=$P$G
 
